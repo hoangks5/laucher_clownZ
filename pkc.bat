b39041efa637dfa21e45aef305a0c06e34ca06b7
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2021-10-09 10:33:33 +0700

    Add files via upload

---
+++ pkc.bat
@@ -0,0 +1,3 @@
+start Dayz_start64.exe -connect=18.138.124.141 -port=2326 "-cpuCount=8" "-mod=@ATM;@Autorun;@Basebuildingplus;@Cannabisplus;@CF;@Code-Lock;@Community-Online-Tools;@Kod_Weapons;@pvzmod-spawn-system;@Spawnselection;@Trader;@unlimitedstamina;@Vppadmintools;@Vppnotifications;@Schanamodglobalchat;@Schanamodparty;@Op-baseitems;@Traderfixesandfeatures;@ServerInfo;@Statbars;@Vanillaplusplusmap"
+
+exit
